76d4d99e985589e91de5f173e5cad578067f3a18
CEONGKITONG <kt.ceong@jerajinsolution.com>
2023-04-06 14:57:27 +0900

    Webjjang Spring ver.2023.03
    Webjjang Spring ver.2023.03 03-03 DBCP 테스트(웹짱과 함께하는 스프링)
     - try (Connection con = dataSource.getConnection()){
         => <bean id="dataSource" class="com.zaxxer.hikari.HikariDataSource" destroy-method="close">
    	 => 위 destroy-method 설정으로 인해 con 객체 종료 시 data close 처리 된다.
    	 => try (객체 선언 생성) { ~~ } catch () {~~~}   => 객체 자동 닫기
      -  Connection 하나당 Oracle 서버의 프로세스 한개 (1:1)
      -  개발자 개별로 Connection Pool 을 구성하여 사용 시 Oracle 서버의 Connection 프로세스가 1:1 생성된다
      -  Oracle 서버 Connection 프로세스 부족 시 Oracle에서 프로세스 생성 갯수 변경 필요

---
+++ spring/ex00/src/test/java/org/zerock/persistence/DataSourceTest.java
@@ -0,0 +1,43 @@
+package org.zerock.persistence;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+//테스트 프로그램을 지정
+@RunWith(SpringJUnit4ClassRunner.class)
+//설정 파일을 불러와서 적용시킨다.
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j  // system.out 대신 사용
+public class DataSourceTest {
+	
+	@Setter(onMethod_ = @Autowired)
+	private DataSource dataSource;
+	
+	@Test
+	public void testConnection() {
+		// try (객체 선언 생성) { ~~ } catch () {~~~}   => 객체 자동 닫기 
+		try (Connection con = dataSource.getConnection()){
+			String sql = "SELECT SYSDATE FROM DUAL";
+			PreparedStatement psmt = con.prepareStatement(sql);
+			ResultSet rs = psmt.executeQuery();
+			rs.next();
+			log.info(rs.getString(1));
+			log.info(con);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+}
