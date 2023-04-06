8a682ed6eae3bf2ae792f10ce8e5a558bcd058a3
CEONGKITONG <kt.ceong@jerajinsolution.com>
2023-04-06 13:07:09 +0900

    Webjjang Spring ver.2023.03
    Webjjang Spring ver.2023.03 03-01 오라클 JDBC 연결과 테스트(웹짱과 함께하는 스프링)
    
    *. Oracle XE 설치 시 롤백 오류 현상 발생시
    
     - 윈도우 사용자명이 한글이 아닌 영문으로 되어야함
       기존 한글 사용자이름으로 되어있을 경우 윈도우에 새 계정 추가해서 영문 계정으로 등록해야함
    	Windows 로고키 + R키 -> netplwiz  -> 추가 -> 윈도우 재부팅 -> 추가된 계정으로 로그인 -> Oracle 설치
       관련 참조 : https://answers.microsoft.com/ko-kr/windows/forum/all/windows-10-%ec%97%90%ec%84%9c/ebb6ad55-e69c-45a2-a194-ab38d6fd69c3
     - PC 이름이 15자 이하의 영문자로 되어야함
    
        기타 설치 관련 참조 내용 https://velog.io/@ynjch97/Oracle-%EC%98%A4%EB%9D%BC%ED%81%B4-Database-11g-%EC%A0%91%EC%86%8D-%EC%98%A4%EB%A5%98-%EB%B0%8F-%EC%84%A4%EC%B9%98-%EC%82%AD%EC%A0%9C
                         https://forums.oracle.com/ords/apexds/post/can-t-install-oracle-database-xe-4205
    
    *. tomcat 실행 시 오류 발생
      Caused by: java.lang.IllegalArgumentException: 지정된 주요 리소스셋 [C:\jerajin_study\spring\.metadata\.plugins\org.eclipse.wst.server.core\tmp0\wtpwebapps\ex00]은(는) 유효하지 않습니다.
      Mavent update 로 해결
      또는 workspace 가 다른 곳을 보고 있는지 확인

---
+++ spring/ex00/src/test/java/org/zerock/persistence/JDBCTests.java
@@ -0,0 +1,38 @@
+package org.zerock.persistence;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class JDBCTests {
+	
+	@Test
+	public void testConnection() {
+		
+		Connection con = null;
+		
+		try {
+			//1. 드라이버 확인
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			log.info("드라이버 확인");
+			//2. 연결
+			con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "scott", "tiger");
+			log.info("연결 성공");
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		} finally {
+			try {
+				con.close();				
+			} catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
