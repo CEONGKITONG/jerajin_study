b3aabb704c8d1e82b3295f80a0d8d0c8cca2861b
CEONGKITONG <kt.ceong@jerajinsolution.com>
2023-04-07 14:50:46 +0900

    Webjjang Spring ver.2023.03
    Webjjang Spring ver.2023.03 06-02 BoardController를 이용한 게시판 흐름 만들기 실습(웹짱과 함께하는 스프링)
    	- 게시판 (7개의 url 필요하다) - 개발전에 케이스별 url이 정의 되어야한다.	 (항목, url, next url, 접근 db 테이블 정의... )
    		1) 리스트
    		/board/list.do
    		2) 보기
    		/board/view.do?no=10
    		3-1) 쓰기 폼
    		/board/write.do - get
    		3-2) 쓰기 처리
    		/board/write.do - post (똑같이 사용하면 오류나므로 POST로 분리)
    		4-1) 수정
    		/board/update.do - get
    		4-2) 수정 폼
    		/board/update.do - post (똑같이 사용하면 오류나므로 POST로 분리)
    		5) 삭제
    		/board/delete.do - post (비밀번호가 포함되어 있기 때문에)
    
    	- method return type String = URL 에 .do 를 사용하기 위해서 String 타입으로 진행

---
+++ spring/ex01/src/main/java/org/zerock/board/controller/BoardController.java
@@ -0,0 +1,67 @@
+package org.zerock.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.log4j.Log4j;
+
+//자동 생성 어노테이션 - @Coontroller, @Service, @Repository, @Component, @RestController, @Advice
+//@Coontroller - servlet-context.xml 에 선언, 그 외 어노테이션 - root-context.xml에 선언
+
+@Controller
+@RequestMapping("/board")
+@Log4j
+public class BoardController {
+	
+	@RequestMapping("/list.do")
+	public String list() {
+		log.info("게시판 리스트................................");
+		return "board/list";
+	}
+	
+	@RequestMapping("/view.do")
+	public String view() {
+		log.info("게시판 글보기................................");
+		return "board/view";
+	}
+
+
+	@GetMapping("/write.do")
+	public String writeForm() {
+		log.info("게시판 글쓰기 폼................................");
+		return "board/write";
+	}
+
+	
+
+	@PostMapping("/write.do")	
+	public String write() {
+		log.info("게시판 글쓰기 처리................................");
+		return "redirect:list.do";
+	}
+
+	
+	@GetMapping("/update.do")
+	public String updateForm() {
+		log.info("게시판 수정 폼................................");
+		return "board/update";
+	}
+
+	
+	@PostMapping("/update.do")
+	public String update() {
+		log.info("게시판 글수정 처리................................");
+		return "redirect:view.do";
+	}
+
+	
+	@PostMapping("/delete.do")
+	public String delete() {
+		log.info("게시판 글 삭제 처리................................");
+		return "redirect:list.do";
+	}
+	
+
+}
