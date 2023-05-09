015fd8c0cce051528d2374c39114659a5066d8ba
CEONGKITONG <kt.ceong@jerajinsolution.com>
2023-05-09 15:57:06 +0900

    WPF & MVVM/ Modern Main UI Design (Part 2/2)
    WPF & MVVM/ Modern Main UI Design (Part 2/2) - Header Design, Open & Switch Child Views

--- dotnet/WPFMVVMLogin/WPFMVVMLogin/View/MainView.xaml.cs
+++ dotnet/WPFMVVMLogin/WPFMVVMLogin/View/MainView.xaml.cs
@@ -45,5 +45,23 @@ namespace WPFMVVMLogin.View
         {
             this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
         }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void btnMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal )
+                this.WindowState = WindowState.Maximized;
+            else
+                this.WindowState = WindowState.Normal;
+        }
     }
 }
--- dotnet/WPFMVVMLogin/WPFMVVMLogin/ViewModel/MainViewModel.cs
+++ dotnet/WPFMVVMLogin/WPFMVVMLogin/ViewModel/MainViewModel.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using FontAwesome.Sharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 using WPFMVVMLogin.Models;
 using WPFMVVMLogin.Repositories;
 
@@ -13,7 +15,15 @@ namespace WPFMVVMLogin.ViewModel
     public class MainViewModel : ViewModelBase
     {
         private UserAccountModel _currentUserAccount;
+        private ViewModelBase _currentChildView;
+        private string _caption;
+        private IconChar _icon;
+
+
         private IUserRepository userRepository;
+
+
+        // --> Property
 
         public UserAccountModel CurrentUserAccount
         {
@@ -29,11 +39,81 @@ namespace WPFMVVMLogin.ViewModel
             }
         }
 
+        public ViewModelBase CurrentChildView
+        {
+            get
+            {
+                return _currentChildView;
+            }
+
+            set
+            {
+                _currentChildView = value;
+                OnPropertyChanged(nameof(CurrentChildView));
+            }
+        }
+
+        public string Caption
+        {
+            get
+            {
+                return _caption;
+            }
+
+            set
+            {
+                _caption = value;
+                OnPropertyChanged(nameof(Caption));
+            }
+        }
+
+        public IconChar Icon
+        {
+            get
+            {
+                return _icon;
+            }
+
+            set
+            {
+                _icon = value;
+                OnPropertyChanged(nameof(Icon));
+            }
+        }
+
+        // --> Commands
+        public ICommand ShowHomeViewCommand { get;  }
+        public ICommand ShowCustomerViewCommand { get; }
+
         public MainViewModel()
         {
             userRepository = new UserRepository();
             CurrentUserAccount = new UserAccountModel();
+
+            //Initialize Command
+            ShowHomeViewCommand = new ViewModelCommand(ExecuteShowHomeViewCommand);
+            ShowCustomerViewCommand = new ViewModelCommand(ExecuteShowCustomerViewCommand);
+
+
+            // Default  View
+            ExecuteShowHomeViewCommand(null);
+
+
             LoadCurrentUserData();
+        }
+
+        private void ExecuteShowCustomerViewCommand(object obj)
+        {
+            CurrentChildView = new CustomerViewModel();
+            Caption = "Customers";
+            Icon = IconChar.UserGroup;
+        }
+
+        private void ExecuteShowHomeViewCommand(object obj)
+        {
+            CurrentChildView = new HomeViewModel();
+            Caption = "Dashboard";
+            Icon = IconChar.Home;
         }
 
         private void LoadCurrentUserData()
