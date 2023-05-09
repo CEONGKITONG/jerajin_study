2e947975dff226a5abd849b9dfbdb5a389c09f08
CEONGKITONG <kt.ceong@jerajinsolution.com>
2023-05-09 14:20:53 +0900

    WPF & MVVM/ Modern Main UI Design (Part 1/2)
    #. WPF & MVVM/ Modern Main UI Design (Part 1/2) - Repository of Styles, Menu Buttons, Icons, Drag...

--- dotnet/WPFMVVMLogin/WPFMVVMLogin/View/MainView.xaml.cs
+++ dotnet/WPFMVVMLogin/WPFMVVMLogin/View/MainView.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -8,6 +9,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
@@ -21,7 +23,27 @@ namespace WPFMVVMLogin.View
     {
         public MainView()
         {
-            InitializeComponent();
+            InitializeComponent();            
+        }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+
+        private void pntControlBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            // 창 최대 시 윈도우 드래그 했을 때 창 크키가 원복 안되는 현상을 위한 처리 (Window Message 전송)
+            WindowInteropHelper helper = new WindowInteropHelper(this);
+            // 161 : WM_NCLBUTTONDOWN 커서가 창의 비클라이언트 영역 내에 있는 동안 사용자가 마우스 왼쪽 단추를 누를 때 게시됩니다.
+            //  2  : 16 비트
+            //  0  : 전송 메세지
+            SendMessage(helper.Handle, 161, 2, 0);
+            //DragMove();
+        }
+
+        private void pntControlBar_MouseEnter(object sender, MouseEventArgs e)
+        {
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
         }
     }
 }
