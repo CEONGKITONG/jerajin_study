fd3b72b900be9da6e24e38bb1a8692b7d754952c
CEONGKITONG <kt.ceong@jerajinsolution.com>
2023-05-09 09:20:18 +0900

    Create Login in WPF, MVVM Pattern
    Create Login in WPF, MVVM Pattern, C# and SQL Server - Step by Step + Display user data

---
+++ dotnet/WPFMVVMLogin/WPFMVVMLogin/Repositories/RepositoryBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFMVVMLogin.Repositories
+{
+    public class RepositoryBase
+    {
+        private readonly string _connectString;
+        public RepositoryBase()
+        {
+            _connectString = "Server=(local); Database=MVVMLoginDb; Integrated Security=true";
+        }
+
+        protected SqlConnection GetConnection()
+        {
+            return new SqlConnection(_connectString);
+        }
+    }
+}
---
+++ dotnet/WPFMVVMLogin/WPFMVVMLogin/ViewModel/MainViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using WPFMVVMLogin.Models;
+using WPFMVVMLogin.Repositories;
+
+namespace WPFMVVMLogin.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private UserAccountModel _currentUserAccount;
+        private IUserRepository userRepository;
+
+        public UserAccountModel CurrentUserAccount
+        {
+            get
+            {
+                return _currentUserAccount;
+            }
+
+            set
+            {
+                _currentUserAccount = value;
+                OnPropertyChanged(nameof(CurrentUserAccount));
+            }
+        }
+
+        public MainViewModel()
+        {
+            userRepository = new UserRepository();
+            CurrentUserAccount = new UserAccountModel();
+            LoadCurrentUserData();
+        }
+
+        private void LoadCurrentUserData()
+        {
+            var user = userRepository.GetByUsername(Thread.CurrentPrincipal.Identity.Name);
+            if(user != null)
+            {
+                CurrentUserAccount.Username = user.Name;
+                CurrentUserAccount.DisplayName = $"Welcome {user.Name} {user.LastName} ;)";
+                CurrentUserAccount.ProfilePicture = null;
+            }
+            else
+            {
+                CurrentUserAccount.DisplayName = "Invalid user, not logged in";
+                //MessageBox.Show("Invalid user, not logged in");
+                //Application.Current.Shutdown();
+            }
+        }
+    }
+}
